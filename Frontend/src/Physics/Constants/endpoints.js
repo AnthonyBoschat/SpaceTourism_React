5475c4ee59986be8114fa7ecd67c74149d0f91ba
AnthonyBoschat <AnthonyBoschat.dev@hotmail.com>
2025-05-26 22:26:03 +0200

    update endpoint

--- Frontend/src/Physics/Constants/endpoints.js
+++ Frontend/src/Physics/Constants/endpoints.js
@@ -1,4 +1,6 @@
-const backend_url = "http://localhost:8000/api"
+// const backend_url = "http://localhost:8000/api"
+const backend_url = "https://boschat.cefim.o2switch.site/api/api"
+
 
 
 const ENDPOINTS = {
